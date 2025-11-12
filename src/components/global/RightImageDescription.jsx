064bd7d1c53b3c7365f638029813b75039f07016
AutobotMain <autobotphwebappproject@gmail.com>
2025-11-12 17:30:59 +0800

    Update Landing page contents

--- src/components/global/RightImageDescription.jsx
+++ src/components/global/RightImageDescription.jsx
@@ -6,7 +6,7 @@ function RightImageDescription({ rowClassName, DescBoxClass, BoxTittle, BoxDescr
     <div className={`row p-0 ${rowClassName}`}>
         <div className='col-lg-2 col-md-0 col-sm-0 p-0 m-0'></div>
         <div className={`image_desc_div col-lg-10 col-md-12 col-sm-12 m-0 ${DescBoxClass}`}>
-            <h1>{ BoxTittle }</h1>
+            <h3>{ BoxTittle }</h3>
             <p>{ BoxDescription }</p>
             <Link to={RouterLink} className="black_btn">{BtnTittle}</Link>
         </div>
